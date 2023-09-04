57a315d0cbd87db63c54ec351339a4636b6629d3
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-09-04 16:17:32 +0200

    /

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const {express, routes} = require("./controller");
 const cookieParser = require('cookie-parser')
 const path = require("path");
-const port = +process.env.port || 3000;
+const port = +process.env.PORT || 3000;
 const app= express ();
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
